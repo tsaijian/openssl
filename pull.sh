a1cec2d8e942d1d46da68c6c9e7fe61967f17ee6
Caleb <yocalebo@gmail.com>
2023-11-20 15:02:32 -0500

    bump to 3.0.12-2

--- pull.sh
+++ pull.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 PACKAGE="openssl"
 PACKAGE_FIRST_CHAR=$(printf "%s" "$PACKAGE" | cut -c1)
-VERSION=3.0.11
-REVISION=1
+VERSION=3.0.12
+REVISION=2
 
 
 wget http://deb.debian.org/debian/pool/main/$PACKAGE_FIRST_CHAR/$PACKAGE/${PACKAGE}_$VERSION-$REVISION.debian.tar.xz
